4f6cbd0fc130d5646e5b44f948e4fc4e12326fcd
AnnetaDe <anneta.liss@gmail.com>
2024-07-08 17:34:10 +0200

    hide

--- db/initMongoDB.js
+++ db/initMongoDB.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose');
+const DB_HOST = require('../config');
 
 const initMongoDB = async () => {
   try {
-    const DB_HOST =
-      'mongodb+srv://annetaliss:29248@cluster0.yy6y8we.mongodb.net/my_contacts?retryWrites=true&w=majority&appName=Cluster0';
+    // const DB_HOST =
+    //   'mongodb+srv://annetaliss:29248@cluster0.yy6y8we.mongodb.net/my_contacts?retryWrites=true&w=majority&appName=Cluster0';
     await mongoose.connect(DB_HOST);
     console.log('Database connection successful');
   } catch (err) {
