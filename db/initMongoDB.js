a1a3723c5ab1600ce63a18c18ccd765868eccba6
AnnetaDe <anneta.liss@gmail.com>
2024-07-08 17:03:31 +0200

    db connected

---
+++ db/initMongoDB.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const initMongoDB = async () => {
+  try {
+    const DB_HOST =
+      'mongodb+srv://annetaliss:29248@cluster0.yy6y8we.mongodb.net/my_contacts?retryWrites=true&w=majority&appName=Cluster0';
+    await mongoose.connect(DB_HOST);
+    console.log('Database connection successful');
+  } catch (err) {
+    console.log('error db');
+    throw err;
+  }
+};
+
+module.exports = initMongoDB;
